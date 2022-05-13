0225edf0697242c592d8b8215e6c720e9978583a
Andy <andybrummittps4@hotmail.com>
2022-05-13 12:08:12 +0100

    first commit, added navbar and styled responsively

---
+++ src/components/navbar/Navbar.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import "./navbar.styles.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="main-nav">
+        <div className="home-link">
+            <a href="#" className="home-link">
+                <FontAwesomeIcon icon={faHouse} />
+            </a>
+        </div>
+        <ul className="nav-links">
+            <li><a href="#">My Work</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <ul className="info-links">
+            <li><a href="">
+            <FontAwesomeIcon icon={faLinkedin} />
+            </a></li>
+            <li><a href="">
+            <FontAwesomeIcon icon={faGithub} />
+            </a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
