c8df164da33a325d93a34563322011da42c7fe88
Andy <andybrummittps4@hotmail.com>
2022-05-29 13:58:06 +0100

    small ui changes, changed opacity on white sections in about me and portfolio

--- src/components/pagetitle/PageTitle.component.jsx
+++ src/components/pagetitle/PageTitle.component.jsx
@@ -4,7 +4,7 @@ import { useInView } from "react-intersection-observer";
 import "./pagetitle.styles.scss";
 
 const titleVariants = {
-    hidden: { opacity: 0, y: -100 },
+    hidden: { opacity: 0, y: -200 },
     visible: {
       opacity: 1,
       y: 0,
