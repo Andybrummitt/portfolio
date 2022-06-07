d3cfb8f8c7cfd48321a2170e5b49703385335673
Andy <andybrummittps4@hotmail.com>
2022-06-07 12:18:13 +0100

    deleted unused files

--- src/components/home/Home.component.jsx
+++ src/components/home/Home.component.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import AboutMe from '../aboutme/AboutMe.component'
 import LandingPage from '../landingpage/LandingPage.component'
 import Portfolio from '../portfolio/Portfolio.component'
-import Test from '../test/Test.component'
 
 const Home = () => {
   return (
