f0891daabe6db68537f15a94296d914370639245
Andy <andybrummittps4@hotmail.com>
2022-06-07 16:22:21 +0100

    added submit btn and netlify forms stuff for contact page

--- src/components/contact/Contact.component.jsx
+++ src/components/contact/Contact.component.jsx
@@ -1,34 +1,58 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
 import PageTitle from "../pagetitle/PageTitle.component";
 import "./contact.styles.scss";
 
-const Contact = () => {
-  const formVariants = {
-    hidden: { scale: 0 },
-    visible: {
-      scale: 1,
-      transition: {
-        type: "spring",
-        delay: 0.5,
-        duration: 1.5,
-        delayChildren: 1.5,
-        staggerChildren: 0.5,
-      },
+const formVariants = {
+  hidden: { scale: 0 },
+  visible: {
+    scale: 1,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+      duration: 1.5,
+      delayChildren: 1.5,
+      staggerChildren: 0.5,
     },
-  };
+  },
+};
 
-  const childVariants = {
-    hidden: { x: -20, opacity: 0 },
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: {
-        type: "spring",
-        duration: 0.3,
-      },
+const childVariants = {
+  hidden: { x: -20, opacity: 0 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      duration: 0.3,
     },
-  };
+  },
+};
+
+const buttonVariants = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.3,
+    },
+  },
+};
+
+const Contact = () => {
+
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ message, setMessage ] = useState('');
+
+  const handleSubmit = () => {
+
+  }
 
   return (
     <div className="contact-page-container">
@@ -36,7 +60,6 @@ const Contact = () => {
       <div className="contact-content-container">
         <motion.form
           name="contact"
-          onSubmit="submit"
           data-netlify="true"
           className="contact-form"
           method="POST"
@@ -44,17 +67,20 @@ const Contact = () => {
           animate="visible"
           variants={formVariants}
         >
+        <input type="hidden" name="form-name" value="contact"/>
           <motion.input
             variants={childVariants}
             type="text"
             name="name"
             placeholder="Name"
+            value={name} onChange={(e) => setName(e.target.value)}
             required
           />
           <motion.input
             variants={childVariants}
             type="email"
             name="email"
+            value={email} onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
             required
           />
@@ -62,9 +88,13 @@ const Contact = () => {
             variants={childVariants}
             placeholder="Message"
             name="message"
+            value={message} onChange={(e) => setMessage(e.target.value)}
             rows={6}
             required
           />
+          <motion.button variants={buttonVariants} type="submit">
+            Submit
+          </motion.button>
         </motion.form>
       </div>
     </div>
