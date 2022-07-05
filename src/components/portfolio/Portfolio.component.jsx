7b03f9c5a7ec0eeab51bf527c7c62c01999f2f4c
Andy <andybrummittps4@hotmail.com>
2022-07-05 16:33:59 +0100

    small ui fix with portfolio component projects now at top

--- src/components/portfolio/Portfolio.component.jsx
+++ src/components/portfolio/Portfolio.component.jsx
@@ -1,8 +1,3 @@
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { AnimatePresence } from "framer-motion";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import PageTitle from "../pagetitle/PageTitle.component";
 import "./portfolio.styles.scss";
@@ -10,12 +5,13 @@ import Project from "./projects/Project.component";
 import projectObjs from "./projects/projectObjs";
 
 const Portfolio = () => {
-
   return (
     <div className="portfolio-container" id="portfolio">
       <PageTitle text={"Portfolio"} />
       <div className="portfolio-content-container">
-        {projectObjs.map(project => <Project key={uuidv4()} info={project} /> )}
+        {projectObjs.map((project) => (
+          <Project key={uuidv4()} info={project} />
+        ))}
       </div>
     </div>
   );
--- src/components/portfolio/projects/Project.component.jsx
+++ src/components/portfolio/projects/Project.component.jsx
@@ -1,14 +1,42 @@
-import { useState } from "react";
+import { useAnimation } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import { v4 as uuidv4 } from "uuid";
 import "./project.styles.scss";
 
 const Project = ({ info }) => {
   const { image, title, about, website, github, technologies } = info;
 
+  const sectionVariants = {
+    hidden: { y: "100vh", opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
+  const controls = useAnimation();
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start(sectionVariants.visible);
+    }
+    if (!inView) {
+      controls.start(sectionVariants.hidden);
+    }
+  }, [inView]);
+
   const [imageLoaded, setImageLoaded] = useState(false);
 
   return (
-    <div className="project-container">
+    <div className="ref-container">
+      <div className="project-container">
       <div className="inner-container">
         <h3 className="project-title">{title}</h3>
         <section className="image-container">
@@ -46,6 +74,7 @@ const Project = ({ info }) => {
             </button>
           ) : null}
         </div>
+      </div>
       </div>
     </div>
   );
