884ce935ce89577fb1eda0be383f0205b9ffa28f
Andy <andybrummittps4@hotmail.com>
2022-07-06 17:21:45 +0100

    added mobile images and changed format of portfolio projects

--- src/components/portfolio/projects/Project.component.jsx
+++ src/components/portfolio/projects/Project.component.jsx
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 import "./project.styles.scss";
 
 const Project = ({ info }) => {
-  const { image, title, about, website, github, technologies } = info;
+  const { mainImage, mobileImage, title, about, website, github, technologies } = info;
 
   const sectionVariants = {
     hidden: { opacity: 0 },
@@ -30,33 +30,41 @@ const Project = ({ info }) => {
   }, [inView]);
 
   const [imageLoaded, setImageLoaded] = useState(false);
+  const [ mobileImageLoaded, setMobileImageLoaded ] = useState(false);
 
   return (
     <div className="ref-container" ref={ref}>
       <motion.div className="project-container" animate={controls}>
       <div className="inner-container">
-        <h3 className="project-title">{title}</h3>
+        <h1 className="project-title">{title}</h1>
         <section className="image-container">
           <img
-            src={image}
+            src={mainImage}
             alt="project-image"
             className={`${imageLoaded ? "" : "hide"}`}
             onLoad={() => setImageLoaded(true)}
           />
         </section>
-
         <section className="about-project">
-          <div className="technologies-used">
-            <h3>Technologies Used:</h3>
-            <ul>
-              {technologies.map((elem) => (
-                <li key={uuidv4()}>{elem}</li>
-              ))}
-            </ul>
-          </div>
+          <div className="inner-about-container">
+            <div className="technologies-used">
+              <h3>Technologies Used:</h3>
+              <ul>
+                {technologies.map((elem) => (
+                  <li key={uuidv4()}>{elem}</li>
+                ))}
+              </ul>
+            </div>
+          <img
+                src={mobileImage}
+                alt="project-image-mobile"
+                className={`${mobileImageLoaded ? "" : "hide"}`}
+                onLoad={() => setMobileImageLoaded(true)}
+          />
           <div className="more-information">
             <h3>More information</h3>
             <p>{about}</p>
+          </div>   
           </div>
         </section>
         <div className="project-links-container">
--- src/components/portfolio/projects/projectObjs.js
+++ src/components/portfolio/projects/projectObjs.js
@@ -1,8 +1,13 @@
 import mixin from '../../../images/project-images/mixin.png';
+import mixinMobile from '../../../images/project-images/mixin-mobile.png';
 import battleship from '../../../images/project-images/battleship-game.png';
+import battleshipMobile from '../../../images/project-images/battleship-game-mobile.png';
 import chatty from '../../../images/project-images/chatty.png';
+import chattyMobile from '../../../images/project-images/chatty-mobile.png';
 import andysPizza from '../../../images/project-images/andys-pizza.png';
+import andysPizzaMobile from '../../../images/project-images/andys-pizza-mobile.png'
 import tagMyTunes from '../../../images/project-images/tag-my-tunes.png';
+import tagMyTunesMobile from '../../../images/project-images/tag-my-tunes-mobile.png';
 
 const projectObjs = [
     {
@@ -12,8 +17,8 @@ const projectObjs = [
         about: 'My most recent project is a file ID3 tagger for mp3 files. When converting music videos to mp3s the title and artist fields are often not populated correctly. Users can upload up to 100 mp3 files and the server will populate those fields to its best guess and remove unwanted strings in the file name. The user then receives a zip file to download with the altered files. I\'ve decided to keep the source code in a private github repository as I want to deploy this application with proper hosting.',
         website: 'https://tagmytunes.herokuapp.com/',
         github: null,
-        image: tagMyTunes
-
+        mainImage: tagMyTunes,
+        mobileImage: tagMyTunesMobile
     },
     {
         key: 2,
@@ -22,7 +27,8 @@ const projectObjs = [
         about: 'This app is a single page application that gives users information about cocktail ingredients and recipes. The user can input ingredients and see cocktails that they can make or ingredients that they need. They can search specific cocktails by ingredient or name and more. The app uses the cocktailDB API where the client makes calls to the express server which calls the cocktailDB REST API which in turn returns the data to the client through the server. I wanted to build a SPA with vanilla javascript to learn more about how React and React Router works under the hood.',
         website: 'http://mixinapp.herokuapp.com/',
         github: 'https://github.com/Andybrummitt/cocktailapp',
-        image: mixin
+        mainImage: mixin,
+        mobileImage: mixinMobile
     },
     {
         key: 3,
@@ -31,7 +37,8 @@ const projectObjs = [
         about: 'This project is a single player battleship game where the user can play against a bot. The user places their ships on selected tiles (made with an html table element) and can guess which tiles the bot has chosen. The bot then has their turn and so on.',
         website: 'https://battleshipjsgame.herokuapp.com/',
         github: 'https://github.com/Andybrummitt/battleship',
-        image: battleship
+        mainImage: battleship,
+        mobileImage: battleshipMobile
     },
     {
         key: 4,
@@ -40,7 +47,8 @@ const projectObjs = [
         about: 'This app is a serverless chat app similar to facebook messenger. The user can authenticate with google or sign in with their own email and password and can search for users by username to chat to.',
         github: 'https://github.com/Andybrummitt/chatApp',
         website: null,
-        image: chatty
+        mainImage: chatty,
+        mobileImage: chattyMobile
     },
     {
         key: 5,
@@ -49,7 +57,8 @@ const projectObjs = [
         about: 'This project is a mock restaurant website. Users can "book a table" and will receive a booking confirmation email. This was built with EJS templating on the back end.',
         github: 'https://github.com/Andybrummitt/andyspizza',
         website: 'http://andyspizza.herokuapp.com/',
-        image: andysPizza
+        mainImage: andysPizza,
+        mobileImage: andysPizzaMobile
     }
 ]
 
