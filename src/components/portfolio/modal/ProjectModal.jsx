610c4ff3e6f1d925f24f4e8844e514188268e26c
Andy <andybrummittps4@hotmail.com>
2022-07-20 09:47:48 +0100

    removed scale modals, strange behaviour on safari

--- src/components/portfolio/modal/ProjectModal.jsx
+++ src/components/portfolio/modal/ProjectModal.jsx
@@ -14,8 +14,8 @@ const ProjectModal = ({ projectInfo, showModal, setShowModal }) => {
       {showModal && (
         <>
           <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
             className="project-modal"
           >
